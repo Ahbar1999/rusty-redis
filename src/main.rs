d376a1d18b92eeea2886087a36ccf9b894c3d7c9
Ahbar <ahbar@Ahbars-MacBook-Air.local>
2025-07-06 16:00:53 +0530

    third task

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
 #![allow(unused_imports)]
-use std::{io::Write, net::TcpListener};
+use std::{io::{Read, Write}, net::TcpListener};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+
+    let mut input_buf = String::from("");
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
-                _stream.write_all(b"+PONG\r\n").unwrap();
+                _stream.read_to_string(&mut input_buf).unwrap();
+                for cmd in input_buf.split('\n') {
+                    if cmd == "PING" {
+                        _stream.write_all(b"+PONG\r\n").unwrap();
+                    }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
