8a4b1ec3509e1bfa7a9308650a802777e07ec08d
Ahbar <ahbar@Ahbars-MacBook-Air.local>
2025-07-06 16:18:03 +0530

    third task

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{io::{BufRead, BufReader, Read, Write}, net::TcpListener};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+    // println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
@@ -12,9 +12,10 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
+                println!("conn recvd!");
                 // reader = BufReader::new(&_stream).read_to_string(&mut input_buf);
-                // _stream.read_to_string(&mut input_buf).unwrap();
-                // println!("{}", &input_buf);
+                _stream.read_to_string(&mut input_buf).unwrap();
+                println!("{:?}", &input_buf);
                 for line in input_buf.split('\n') {
                     if line == "PING" {
                         _stream.write(b"+PONG\r\n").unwrap();
