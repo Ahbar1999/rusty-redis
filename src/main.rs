96d9b11d16432c897ae3ed4718be0a7a8d9f3fd0
Ahbar <ahbar@Ahbars-MacBook-Air.local>
2025-07-06 16:39:31 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,19 @@ fn main() {
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
-    let mut input_buf = String::from("");
-    let mut reader; 
+    let mut input_buf: Vec<u8> = vec![0; 1024]; 
+    let mut bytes_rx =0;
+
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
-                println!("conn recvd!");
-                reader = BufReader::new(&_stream);
-                reader.read_to_string(&mut input_buf).unwrap();
-                // _stream.read_to_string(&mut input_buf).unwrap();
-                println!("{:?}", &input_buf);
-                for line in input_buf.split('\n') {
-                    if line == "PING" {
-                        _stream.write(b"+PONG\r\n").unwrap();
+                loop {
+                    // READ one PING, SEND one PONG
+                    bytes_rx = _stream.read(&mut input_buf).unwrap();
+                    if bytes_rx == 0 {
+                        break;
                     }
+                    _stream.write(b"+PONG\r\n").unwrap();
                 }
             }
             Err(e) => {
